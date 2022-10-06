46ee6e5fef336278be2d8dfec1aabe55ebfaa91c
nikolai <axroit.08@gmail.com>
2022-10-06 23:15:19 +0400

    start of two-sums on leetCode

---
+++ 01_Two_Sum/app.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numsLength = nums.length;
+    if (!target || !numsLength) return null;
+};
+
+
+const numbers = [2,7,11,15];
+const target = 9;
+console.log(twoSum(numbers, target));
