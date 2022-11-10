9a696dea0a0bcb30386bb69f7b49ecdd38a37c12
Nikolai Maidan <axroit.08@gmail.com>
2022-11-10 22:27:30 +0400

    add my solution of two sum task

--- 01_Two_Sum/app.js
+++ 01_Two_Sum/app.js
@@ -4,11 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const numsLength = nums.length;
-    if (!target || !numsLength) return null;
+    const arrLen = nums.length;
+    let mass = [];
+    for (let i=0; i <= arrLen; i++) {
+      const number = nums[i];
+      for (let q=0; q <= arrLen; q++) {
+        const num = nums[q];
+        if (i === q) continue;
+        if ((number + num) === target && mass.length < 2) {
+          mass.push(i);
+          mass.push(q);
+        }
+      }
+    }
+    return  mass;
 };
 
 
 const numbers = [2,7,11,15];
 const target = 9;
+// const numbers = [3,3];
+// const target = 6;
 console.log(twoSum(numbers, target));
