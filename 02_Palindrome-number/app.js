a33f5bc8f76bdd584a38cfa0effc883f64aacfaa
Nikolai Maidan <axroit.08@gmail.com>
2022-11-11 23:08:32 +0400

    success with solution

--- 02_Palindrome-number/app.js
+++ 02_Palindrome-number/app.js
@@ -4,9 +4,10 @@
  */
 
 var isPalindrome = function(x) {
-
+  if (x < 0) return false;
+  return x.toString() === x.toString().split("").reverse().join("");
 };
 
 
-const x = 121;
-isPalindrome(x);
+// const x = 12;
+// isPalindrome(x);
