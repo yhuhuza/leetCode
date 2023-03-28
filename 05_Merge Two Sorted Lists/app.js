5141d49607ee3947f741acd47bf743dac3cf0952
Nikolai Maidan <axroit.08@gmail.com>
2023-03-28 20:45:08 +0400

    add new task merging to linked lists

---
+++ 05_Merge Two Sorted Lists/app.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+
+};
+
+
+const firstExample = "III";
+// const secondExample = "LVIII";
+// const thirdExample = "MCMXCIV";
+
+romanToInt(firstExample);
+// romanToInt(secondExample);
+// romanToInt(thirdExample);
